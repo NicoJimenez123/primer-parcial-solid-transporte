52e51e7f728619ff122534ab1a3efd10b2bb1591
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-04-25 19:32:12 -0300

    Añadida implementación de la interfaz ITransporteAire

--- primer-parcial-solid-transporte/Avion.cs
+++ primer-parcial-solid-transporte/Avion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace primer_parcial_solid_transporte
 {
-    public class Avion : ITransporte
+    public class Avion : ITransporte, ITransporteAire
     {
         public string _nombre;
         public int _velocidadActual;
